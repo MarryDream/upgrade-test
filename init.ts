f1e9419966cd4e3b05ed0ff8aedde20b434870d1
MarryDream <2037177696@qq.com>
2024-10-13 02:12:06 +0800

    init

---
+++ init.ts
@@ -0,0 +1,11 @@
+import { definePlugin } from "@/modules/plugin";
+
+export default definePlugin( {
+	name: "测试测试",
+	cfgList: [],
+	repo: {
+		owner: "MarryDream",
+		repoName: "upgrade-test",
+		ref: "master"
+	}
+} );
